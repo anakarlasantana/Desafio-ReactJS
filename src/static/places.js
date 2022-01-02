e6879ccfcb2775a93dffc4887609340cf153e7d7
Ana <santanadeanakarla@gmail.com>
2022-01-02 20:27:20 -0300

    first commit

---
+++ src/static/places.js
@@ -0,0 +1,19 @@
+const places = [
+  {
+    title: 'Como funciona',
+    description:
+      'Imagine uma máquina trabalhando em tempo real para fazer a sua empresa ser encontrada em todos os principais mapas e listas da internet? A HubLocal faz isso para sua empresa.',
+    imageUrl: process.env.PUBLIC_URL + '/assets/bg2.png',
+    time: 1500,
+  },
+  {
+    title: 'Transforme sua empresa',
+    description:
+      "HubLocal impulsiona vendas, aumenta o fluxo de clientes e ligações dos pontos de vendas da sua empresa na internet em 50 mecanismos de buscas de forma totalmente automatizada.",
+    imageUrl: process.env.PUBLIC_URL + '/assets/bg.png',
+    time: 1500,
+  },
+  
+];
+
+export default places;
